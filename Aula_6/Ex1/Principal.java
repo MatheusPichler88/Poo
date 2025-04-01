4355c0d15bee501b923d83cd25ba3a95be093fc7
MatheusPichler88 <matheuspichler94@gmail.com>
2025-04-01 10:35:11 -0300

    Add files via upload

---
+++ Aula_6/Ex1/Principal.java
@@ -0,0 +1,16 @@
+package Ex1;
+
+public class Principal {
+    public static void main(String[] args) {
+       public class Aluno a = new Aluno () {
+        a.setNome("Matheus");
+        a.setNota1(8.9);
+        a.setNota2(9.3);
+        }
+
+        System.out.println("Nome:"+a.getNome());
+        System.out.println("nota1:" +a.getNota1());
+        System.out.println("Nota2:"+a.getNota2());
+        System.out.println("Media:"+a.calcularMedia());
+    }
+}
